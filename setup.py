c464579fdc106ff1cd06c867d2ca69dc8ba82add
itay-bardugo <itaybardugo91@gmail.com>
2020-03-04 23:40:38 +0200

    cosmetic changes

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="itay-bardugo/flask_version",
-    version="0.0.1",
+    name="itay_bardugo/flask_version",
+    version="0.0.2",
     author="Itay Bardugo",
     author_email="itaybardugo91@gmail.com",
     description="a package to handle flask routes endpoints by versions",
