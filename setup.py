e55c9986463377a769cf976d1472841e6b764ee5
itay-bardugo <itaybardugo91@gmail.com>
2020-03-04 23:11:03 +0200

    cosmetic changes

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="a package to handle flask routes endpoints by versions",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/itay-bardugo/flask_version",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
