70484994002a8b9a97902b924aaefa22e86aec04
itay-bardugo <itaybardugo91@gmail.com>
2020-03-05 19:26:39 +0200

    unittest cosmetic changes

--- unittest/test_main.py
+++ unittest/test_main.py
@@ -8,10 +8,10 @@ class TestBase(TestCase):
         @register_version_getter
         def get_version():
             return "1.2"
-        self.assertEqual(endpoints.endpoint_a(), "current version!")
+        self.assertEqual(endpoints.endpoint_a(), "current version!", "expected output: current version!")
 
     def test_old_vresion(self):
         @register_version_getter
         def get_version():
             return "1.0"
-        self.assertEqual(endpoints.endpoint_a(), "i am an endpoint for version 1.0")
+        self.assertEqual(endpoints.endpoint_a(), "i am an endpoint for version 1.0", "expected output: i am an endpoint for version 1.0")
